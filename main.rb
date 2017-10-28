3570bd3802b0b52e56d8645931aadb65b92ae70e
christina <christina@mercury.ai>
2017-10-28 13:23:00 +0200

    own configure block for development

--- main.rb
+++ main.rb
@@ -2,12 +2,17 @@ require 'dotenv'
 Dotenv.load
 
 require 'sinatra'
-require 'sinatra/reloader' if development?
 require 'sysrandom/securerandom'
 
 configure do
   enable :sessions
   set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+end
+
+configure(:development) do
+  require 'sinatra/reloader'
+  # And, for example:
+  # also_reload 'lib/database_connection.rb'
 end
 
 #### Routes ####
